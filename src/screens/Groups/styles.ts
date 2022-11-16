7a43bd7b490dc3924f72738cc0915a3a9e3b7b23
monezero <monezerolindo@gmail.com>
2022-11-16 16:29:36 -0300

    componentes adicionados

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+flex: 1;
+background-color: #000000;
+`;
+
+export const Title = styled.Text`
+color: #fff;
+font-size: 32px;
+`;
